74e7c74424760c4bf03e1e76e422003501b0da4b
stokely <adrienne.j.abrams@gmail.com>
2019-01-12 14:21:01 -0600

    updated jaws connection

--- config/connection.js
+++ config/connection.js
@@ -4,16 +4,19 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "AVG2efTG!",
-    database: "burgers_db"
-});
 
-connection.connect(function(err) {
-    if(err) {
+var connection = process.env.JAWSDB_URL ?
+    mysql.createConnection(process.env.JAWSDB_URL) :
+    mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "AVG2efTG!",
+        database: "burgers_db"
+    });
+
+connection.connect(function (err) {
+    if (err) {
         console.log("error connecting" + err.stack);
         return;
     }
